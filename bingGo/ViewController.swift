4501dc084fef3cb1ff697c2219fa9c5817fcf5cd
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-03-27 15:37:34 +0900

    Initial Commit

---
+++ bingGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bingGo
+//
+//  Created by D7703_22 on 2018. 3. 27..
+//  Copyright © 2018년 jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
