3e2f9ecbedfe5ed71ed60ad39b37840a293b75b9
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-03-27 15:54:57 +0900

    bingGoGame

--- bingGo/ViewController.swift
+++ bingGo/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        input.delegate = self
     }
 
+    @IBAction func guess(_ sender: Any) {
+        let number = arc4random_uniform(6)
+        print(number)
+        let numberString = String(number)
+        
+        if input.text == numberString{
+            resultLabel.text = "빙고!!!!"
+        } else{
+            resultLabel.text = "실패!!! Try again!!  숨겨진 숫자는 현재 \(number)"
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        view.endEditing(true)
+        input.backgroundColor = UIColor.green
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        input.resignFirstResponder()
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
